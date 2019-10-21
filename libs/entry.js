8f5ae1431aa1496e126091f2561530d35a340c15
Arnaud GENTY <arnaud.genty@gmail.com>
2019-10-21 07:52:32 +0200

    nsEntry parent

---
+++ libs/entry.js
@@ -0,0 +1,132 @@
+const NS_CONSTANTS = require('./constants');
+class NsEntry {
+  constructor() {
+    this._nsEntOps = NsEntry.getDefaultOptions();
+    this._nsEntStats = NsEntry._nsEntStatsFactory();
+    this._nsEntLine = NsEntry._nsEntLineFactory();
+    this._nsEntStatus = NsEntry._nsEntStatusFactory();
+
+  }
+
+  static getDefaultOptions() {
+    return {
+      type: NsEntry.constructor.name,
+      name: "No name",
+      interval: 5000,
+      bufferSize: 100,
+      debug: false
+    };
+  }
+
+  getOptions() {
+    return this._nsEntOps;
+  }
+
+  static New(entryName,type) {
+    let childName = (entryName !=null && entryName.length > 0) ? entryName : this.prototype.constructor.name;
+    let entry = require(`../${type}/${childName}`);
+    var nsEntry = new entry();
+    return nsEntry;
+  }
+
+  static _nsEntStatusFactory() {
+    return {
+      ctrl: NS_CONSTANTS.CTRL.STOP,
+    };
+  }
+
+  static _nsEntLineFactory() {
+    return {
+      status: NS_CONSTANTS.STATUS.UNKNOWN,
+      beginAt: Date.now(),
+      endAt: null,
+      duration: 0,
+      result: null
+    };
+  }
+
+  static _nsEntStatsFactory() {
+    return {
+      startAt: null,
+      stopAt: null,
+      pauseAt: null,
+      stopAt: null,
+      totalTime: 0,
+      execCount: 0,
+      successCount: 0,
+      failedCount: 0,
+      unknownCount: 0,
+    };
+  }
+
+  setOptions(nsOptions){
+    this._nsEntOps = nsOptions;
+    return this;
+  }
+
+  setName(str) {
+    this.getOptions().name = str;
+    return this;
+  }
+
+  exec(ent) {
+    // FOR CHILDREN
+  }
+
+  async _out(nsEnt) {
+    if (
+      this._nsEntStatus.ctrl === NS_CONSTANTS.CTRL.START &&
+      this._nsEntLine.status !== NS_CONSTANTS.STATUS.INPROGRESS
+    ) {
+      this._nsEntStats.execCount++;
+      let currNsLine = NsEntry._nsEntLineFactory();
+      this._nsEntLine = currNsLine;
+      this._nsEntLine.status = NS_CONSTANTS.STATUS.INPROGRESS;
+      currNsLine.result = this.exec(nsEnt.result);
+      currNsLine.endAt = Date.now();
+      if (this._nsEntLine.status === NS_CONSTANTS.STATUS.INPROGRESS) {
+        this._nsEntStats.unknownCount++;
+        currNsLine.status = NS_CONSTANTS.STATUS.UNKNOWN;
+      }
+      currNsLine.duration = currNsLine.endAt - currNsLine.beginAt;
+      this._nsEntStats.totalTime += currNsLine.duration;
+    }
+  }
+
+  start() {
+    this._nsEntStatus.ctrl = NS_CONSTANTS.CTRL.START;
+    this._nsEntStats.startAt = Date.now();
+    this.init();
+    return this;
+  }
+
+  stop() {
+    this.close();
+  }
+
+  init() {
+    console.log("NSENTRY INIT");
+    return this;
+  }
+
+  close() {
+    console.log("NSENTRY CLOSE");
+    return this;
+  }
+
+  failed() {
+    this._nsEntLine.status = NS_CONSTANTS.STATUS.FAILED;
+    this._nsEntStats.failedCount++;
+  }
+
+  success() {
+    this._nsEntLine.status = NS_CONSTANTS.STATUS.SUCCESS;
+    this._nsEntStats.successCount++;
+  }
+  getStatistics() {
+    return this._nsEntStats;
+  }
+
+}
+
+module.exports = NsEntry;
